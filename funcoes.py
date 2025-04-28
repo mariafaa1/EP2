4fc038000eeab7dab0edb577cdce76737d24efc8
nicole <nicolepas@al.insper.edu.br>
2025-04-28 17:15:46 -0300

    EX6 fail

--- funcoes.py
+++ funcoes.py
@@ -44,3 +44,38 @@ def calcula_pontos_soma(dados_rolados):
         soma += dado
     return soma
 
+
+#EXERCICIO 6
+def calcula_pontos_sequencia_baixa(dados):
+    
+    if len(dados) < 4:
+        return 0
+
+    n = len(dados)
+    for i in range(n):
+        for j in range(n):
+            if j == i:
+                
+        for k in range(n):
+            if k == i or k == j:
+                    
+        for l in range(n):
+            if l == i or l == j or l == k:
+                        
+
+                    
+            possivel_sequencia = sorted([dados[i], dados[j], dados[k], dados[l]])
+            if possivel_sequencia == [1, 2, 3, 4] or possivel_sequencia == [2, 3, 4, 5] or possivel_sequencia == [3, 4, 5, 6]:
+                 return 15
+
+    return 0
+
+# Exemplos de uso:
+print(calcula_pontos_sequencia_baixa([1, 2, 3, 4, 6]))   # Saída: 15
+print(calcula_pontos_sequencia_baixa([5, 4, 3, 2, 1]))   # Saída: 15
+print(calcula_pontos_sequencia_baixa([2, 3, 4, 5]))      # Saída: 15
+print(calcula_pontos_sequencia_baixa([6, 5, 4, 3]))      # Saída: 15
+print(calcula_pontos_sequencia_baixa([1, 1, 2, 3, 4]))   # Saída: 15
+print(calcula_pontos_sequencia_baixa([1, 3, 4, 5, 6]))   # Saída: 0
+print(calcula_pontos_sequencia_baixa([1, 2, 4, 5]))      # Saída: 0
+print(calcula_pontos_sequencia_baixa([1, 2, 3]))        # Saída: 0
