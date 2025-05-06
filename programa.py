6d5c6ff485eefab3ff5f4b4e10d6eadfaf7ecd8f
nicole <nicolepas@al.insper.edu.br>
2025-05-06 17:18:59 -0300

    m

--- programa.py
+++ programa.py
@@ -1,6 +1,23 @@
 import random
 from funcoes import *
-
+cartela_de_pontos = {
+    'regra_simples':  {
+        1:-1,
+        2:-1,
+        3:-1,
+        4:-1,
+        5:-1,
+        6:-1
+    },
+    'regra_avancada' : {
+        'sem_combinacao':-1,
+        'quadra': -1,
+        'full_house': -1,
+        'sequencia_baixa': -1,
+        'sequencia_alta': -1,
+        'cinco_iguais': -1
+    }
+}
 imprime_cartela(cartela_de_pontos)
 
 n = 5
