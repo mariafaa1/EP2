5c626e6a8a3cb06408337614cef8cc846af219c6
nicole <nicolepas@al.insper.edu.br>
2025-05-06 22:41:31 -0300

    EX 13 final

--- programa.py
+++ programa.py
@@ -1,3 +1,42 @@
+import funcoes as f
+
+cartela = {'regra_simples': {i: -1 for i in range(1, 7)},'regra_avancada': {k: -1 for k in ['sem_combinacao', 'quadra', 'full_house','sequencia_baixa', 'sequencia_alta', 'cinco_iguais']}}
+f.imprime_cartela(cartela)
+rodadas = 0
+while -1 in cartela['regra_simples'].values() and rodadas != 12 or -1 in cartela['regra_avancada'].values() and rodadas != 12:
+    dados_rolados = f.rolar_dados(5)
+    dados_guardados = []
+    reroladas = 0
+    rodada_em_andamento = True
+    rodadas +=1
+    invalido = False
+    while rodada_em_andamento:
+        if invalido == False:
+            print(f"Dados rolados: {dados_rolados}")
+            print(f"Dados guardados: {dados_guardados}")
+            print("Digite 1 para guardar um dado, 2 para remover um dado, 3 para rerrolar, 4 para ver a cartela ou 0 para marcar a pontuação:")
+        opcao = input()
+        invalido = False
+        if opcao == '1':
+            print(f"Digite o índice do dado a ser guardado (0 a 4):")
+            resposta = int(input())
+            dados_rolados, dados_guardados = f.guardar_dado(dados_rolados, dados_guardados, resposta)
+
+        elif opcao == '2':
+            print(f"Digite o índice do dado a ser removido (0 a 4):")
+            resposta = int(input())
+            dados_rolados, dados_guardados = f.remover_dado(dados_rolados, dados_guardados, resposta)
+
+        elif opcao == '3':
+            if reroladas < 2:
+                dados_rolados = f.rolar_dados(len(dados_rolados))
+                reroladas += 1
+            else:
+                print("Você já usou todas as rerrolagens.")
+
+        elif opcao == '4':
+            f.imprime_cartela(cartela)
+
 elif opcao == '0':
     print("Digite a combinação desejada:")
     jogada_feita = False
